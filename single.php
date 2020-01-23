41d21dbf268320f1ce0eee874e85986fba533583
var <varenik16@gmail.com>
2020-01-23 11:23:25 +0400

    added

--- single.php
+++ single.php
@@ -14,19 +14,21 @@
                                     <?php the_post_thumbnail('full', array('class' => 'thumbnail-image'));?>
                                 </div>
                                 <?php the_content(''); ?>
-                                <?php
+                                <div class="cabins">
+                                    <?php
                                     $cabins = get_field('cabins');
-                                foreach ($cabins as $cabin): ?>
-                                    <div class="cabin">
-                                        <div class="img-cabin-wrapper">
-                                            <img src="<?php echo $cabin['cabin']['cabin_img']; ?>" />
+                                    foreach ($cabins as $cabin): ?>
+                                        <div class="cabin">
+                                            <div class="img-cabin-wrapper">
+                                                <img src="<?php echo $cabin['cabin']['cabin_img']; ?>" />
+                                            </div>
+                                            <div class="desc-cabin-wrapper">
+                                                <strong><?php echo $cabin['cabin']['cabin_title']; ?></strong>
+                                                <?php echo $cabin['cabin']['cabin_desc']; ?>
+                                            </div>
                                         </div>
-                                        <div class="desc-cabin-wrapper">
-                                            <strong><?php echo $cabin['cabin']['cabin_title']; ?></strong>
-                                            <?php echo $cabin['cabin']['cabin_desc']; ?>
-                                        </div>
-                                    </div>
-                                <?php endforeach; ?>
+                                    <?php endforeach; ?>
+                                </div>
                             </div>
                         </div>
                     <?php endwhile; ?>
